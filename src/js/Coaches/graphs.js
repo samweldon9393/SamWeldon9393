c4b5cf898bc211033c2240c6b9777475392605df
Sam <samweldon9393@gmail.com>
2025-01-04 20:42:30 -0500

    added interactive graph and little write up for coaches

---
+++ src/js/Coaches/graphs.js
@@ -0,0 +1,403 @@
+coaches = [
+    {
+        "coach": "Adrian Griffin",
+        "mood": "Negative",
+        "ratio": -3.1778425655976674,
+        "positive_count": 343,
+        "negative_count": 1090
+    },
+    {
+        "coach": "Billy Donovan",
+        "mood": "Negative",
+        "ratio": -1.8173076923076923,
+        "positive_count": 104,
+        "negative_count": 189
+    },
+    {
+        "coach": "Brian Keefe",
+        "mood": "Negative",
+        "ratio": -1.7474747474747474,
+        "positive_count": 99,
+        "negative_count": 173
+    },
+    {
+        "coach": "Charles Lee",
+        "mood": "Negative",
+        "ratio": -1.5925925925925926,
+        "positive_count": 27,
+        "negative_count": 43
+    },
+    {
+        "coach": "Chauncey Billups",
+        "mood": "Negative",
+        "ratio": -2.0249480249480247,
+        "positive_count": 481,
+        "negative_count": 974
+    },
+    {
+        "coach": "Chris Finch",
+        "mood": "Negative",
+        "ratio": -3.1464872944693574,
+        "positive_count": 669,
+        "negative_count": 2105
+    },
+    {
+        "coach": "Darko Rajakovic",
+        "mood": "Negative",
+        "ratio": -2.8819444444444446,
+        "positive_count": 144,
+        "negative_count": 415
+    },
+    {
+        "coach": "Darvin Ham",
+        "mood": "Negative",
+        "ratio": -4.098191933240612,
+        "positive_count": 3595,
+        "negative_count": 14733
+    },
+    {
+        "coach": "Doc Rivers",
+        "mood": "Negative",
+        "ratio": -2.7404896421845573,
+        "positive_count": 2655,
+        "negative_count": 7276
+    },
+    {
+        "coach": "Erik Spoelstra",
+        "mood": "Negative",
+        "ratio": -2.800557880055788,
+        "positive_count": 1434,
+        "negative_count": 4016
+    },
+    {
+        "coach": "Gregg Popovich",
+        "mood": "Negative",
+        "ratio": -1.9180327868852458,
+        "positive_count": 61,
+        "negative_count": 117
+    },
+    {
+        "coach": "Ime Udoka",
+        "mood": "Negative",
+        "ratio": -3.2448275862068967,
+        "positive_count": 290,
+        "negative_count": 941
+    },
+    {
+        "coach": "J.B. Bickerstaff",
+        "mood": "Negative",
+        "ratio": -2.277227722772277,
+        "positive_count": 101,
+        "negative_count": 230
+    },
+    {
+        "coach": "JJ Redick",
+        "mood": "Negative",
+        "ratio": -1.6198595787362087,
+        "positive_count": 997,
+        "negative_count": 1615
+    },
+    {
+        "coach": "Jacque Vaughn",
+        "mood": "Negative",
+        "ratio": -2.7559808612440193,
+        "positive_count": 209,
+        "negative_count": 576
+    },
+    {
+        "coach": "Jamahl Mosley",
+        "mood": "Negative",
+        "ratio": -2.4329896907216493,
+        "positive_count": 97,
+        "negative_count": 236
+    },
+    {
+        "coach": "Jason Kidd",
+        "mood": "Negative",
+        "ratio": -2.8567567567567567,
+        "positive_count": 2220,
+        "negative_count": 6342
+    },
+    {
+        "coach": "Joe Mazzulla",
+        "mood": "Negative",
+        "ratio": -2.7025316455696204,
+        "positive_count": 316,
+        "negative_count": 854
+    },
+    {
+        "coach": "Jordi Fernández",
+        "mood": "Positive",
+        "ratio": 1.058139534883721,
+        "positive_count": 91,
+        "negative_count": 86
+    },
+    {
+        "coach": "Kenny Atkinson",
+        "mood": "Positive",
+        "ratio": 1.0642201834862386,
+        "positive_count": 116,
+        "negative_count": 109
+    },
+    {
+        "coach": "Mark Daigneault",
+        "mood": "Negative",
+        "ratio": -1.5625,
+        "positive_count": 32,
+        "negative_count": 50
+    },
+    {
+        "coach": "Michael Malone",
+        "mood": "Negative",
+        "ratio": -2.493595217762596,
+        "positive_count": 1171,
+        "negative_count": 2920
+    },
+    {
+        "coach": "Mike Brown",
+        "mood": "Negative",
+        "ratio": -2.2442396313364057,
+        "positive_count": 217,
+        "negative_count": 487
+    },
+    {
+        "coach": "Mike Budenholzer",
+        "mood": "Negative",
+        "ratio": -3.024126455906822,
+        "positive_count": 1202,
+        "negative_count": 3635
+    },
+    {
+        "coach": "Monty Williams",
+        "mood": "Negative",
+        "ratio": -2.6639676113360324,
+        "positive_count": 2223,
+        "negative_count": 5922
+    },
+    {
+        "coach": "Nick Nurse",
+        "mood": "Negative",
+        "ratio": -2.969767441860465,
+        "positive_count": 1720,
+        "negative_count": 5108
+    },
+    {
+        "coach": "Quin Snyder",
+        "mood": "Negative",
+        "ratio": -2.1601685985247627,
+        "positive_count": 949,
+        "negative_count": 2050
+    },
+    {
+        "coach": "Rick Carlisle",
+        "mood": "Negative",
+        "ratio": -2.7477203647416415,
+        "positive_count": 329,
+        "negative_count": 904
+    },
+    {
+        "coach": "Steve Clifford",
+        "mood": "Negative",
+        "ratio": -3.13953488372093,
+        "positive_count": 129,
+        "negative_count": 405
+    },
+    {
+        "coach": "Steve Kerr",
+        "mood": "Negative",
+        "ratio": -2.1089681774349085,
+        "positive_count": 1037,
+        "negative_count": 2187
+    },
+    {
+        "coach": "Taylor Jenkins",
+        "mood": "Negative",
+        "ratio": -2.8048245614035086,
+        "positive_count": 456,
+        "negative_count": 1279
+    },
+    {
+        "coach": "Tom Thibodeau",
+        "mood": "Negative",
+        "ratio": -2.9047619047619047,
+        "positive_count": 84,
+        "negative_count": 244
+    },
+    {
+        "coach": "Tyronn Lue",
+        "mood": "Negative",
+        "ratio": -3.3321428571428573,
+        "positive_count": 280,
+        "negative_count": 933
+    },
+    {
+        "coach": "Wes Unseld Jr",
+        "mood": "Negative",
+        "ratio": -1.8284883720930232,
+        "positive_count": 344,
+        "negative_count": 629
+    },
+    {
+        "coach": "Will Hardy",
+        "mood": "Negative",
+        "ratio": -1.2454545454545454,
+        "positive_count": 110,
+        "negative_count": 137
+    },
+    {
+        "coach": "Willie Green",
+        "mood": "Negative",
+        "ratio": -2.2708333333333335,
+        "positive_count": 96,
+        "negative_count": 218
+    }
+]
+
+
+
+function chart()  {
+    // Declare the chart dimensions and margins.
+    const width = 400;
+    const height = 450;
+    const marginTop = 30;
+    const marginRight = 0;
+    const marginBottom = 0;
+    const marginLeft = 40;
+
+    // Declare the x (horizontal position) scale.
+    const x = d3.scaleBand()
+        .domain(d3.groupSort(coaches, ([d]) => -d.ratio, (d) => d.coach)) // descending frequency
+        .range([marginLeft, width - marginRight])
+        .padding(0.1);
+
+    // Declare the y (vertical position) scale.
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(coaches, (d) => d.positive_count)])
+        .range([height - marginBottom, marginTop]);
+
+    // Create the SVG container.
+    const svg = d3.select("svg");
+
+    // Add a rect for each bar.
+    svg.append("g")
+        .attr("fill", "red")
+        .selectAll()
+        .data(coaches)
+        .join("rect")
+        .attr("x", (d) => x(d.coach))
+        .attr("y", (d) => y(d.negative_count))
+        .attr("height", (d) => y(1) - y(d.negative_count))
+        .attr("width", x.bandwidth());
+    // Add a rect for each bar.
+    svg.append("g")
+        .attr("fill", "steelblue")
+        .selectAll()
+        .data(coaches)
+        .join("rect")
+        .attr("x", (d) => x(d.coach))
+        .attr("y", (d) => y(d.positive_count))
+        .attr("height", (d) => y(1) - y(d.positive_count))
+        .attr("width", x.bandwidth());
+
+
+    // Add the x-axis and label.
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+
+    // Add the y-axis and label, and remove the domain line.
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(d3.axisLeft(y).tickFormat((y) => (y * 1).toFixed()))
+        .call(g => g.select(".domain").remove())
+        .call(g => g.append("text")
+            .attr("x", -marginLeft)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text("↑ Ratio (%)"));
+
+    d3.select("div")
+        .on('click', function(e, d) {
+            d3.select('h4')
+                .text(`Reddit Hates Coaches`)
+        });
+    d3.selectAll("rect")
+        .on('mouseover', function(e, d) {
+            d3.select('h4')
+                .text(`${d.coach}: ${d.positive_count} Positive / ${d.negative_count} Negative`)
+        });
+
+
+    // Return the SVG element.
+    return svg.node();
+}
+chart();
+
+//function info(){
+//  const svg = d3.selectAll('svg');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //attach event listeners to the initial buttons
+    const info = document.querySelector('.more-info');
+    const svg = document.querySelector('svg');
+    const div = document.getElementById('coach-graphs');
+    info.addEventListener('click', (event) => {
+        div.innerHTML = '';
+
+        const header = document.createElement('h3');
+        header.textContent = 'Reddit Hates Coaches';
+        header.classList.add('text-center');
+        header.classList.add('mt-2');
+        
+        const expl = document.createElement('p');
+        expl.classList.add('text-center');
+        expl.classList.add('text-sm');
+        expl.classList.add('mx-4');
+        expl.classList.add('mt-12');
+        expl.classList.add('mb-6');
+        expl.textContent = 'Sentiment analysis of Reddit comments about NBA head coaches. ' +
+            'The graph displays negative comments in red and positive comments in blue, ' +
+            'it is sorted by ratio, so for example, the two leftmost coaches are the only ones to ' +
+            'recieve more positive than negative comments. The difference in bar sizes demonstrates ' +
+            'a limitation of the data, but it also provides interesting insights into the impact of ' +
+            'market size and tenure on coach popularity. ' +
+            'Hover over a bar on the graph to see which coach is being represented. ' +
+            'Approximately 100,000 comments were scraped and analyzed for this project ' +
+            'Analysis used a pre-trained model, and the data collection was a bit crude ' +
+            'due to technical/time limitations, but the results do track very closely to ' +
+            'expected values based on anecdotal knowledge of public sentiment surrounding subjects. '+
+            '\nLearn more on:';
+
+
+        const link = document.createElement('a');
+        link.href = "https://github.com/samweldon9393/Reddit-Hates-Coaches";
+        link.classList.add('ml-44');
+        link.classList.add('mt-2');
+        link.classList.add('go-back');
+        link.textContent = 'GitHub';
+
+        const back = document.createElement('h3');
+        back.setAttribute('id', 'go-back');
+        back.textContent = 'See Graph Again';
+        back.classList.add('go-back');
+        back.classList.add('text-center');
+        back.classList.add('mt-2');
+        
+        div.appendChild(header);
+        div.appendChild(expl);
+        div.appendChild(link);
+        div.appendChild(back);
+
+
+        back.addEventListener('click', (event) => {
+            location.reload();
+            
+        });
+
+
+    });
+    
+});
+
+
